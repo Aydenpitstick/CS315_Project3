8ccc57e532284d1b9bd5db3f49600b42f42401d2
Aydenpitstick <108236392+Aydenpitstick@users.noreply.github.com>
2025-12-09 17:55:51 -0500

    Messed Up Upload

--- cs315project3/cs315project3/Assets/Scripts/lever.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		get_node("/root/GameController").leverPull()
-		$AnimatedSprite2D.play("After")
-		$CollisionShape2D.queue_free()
-		
